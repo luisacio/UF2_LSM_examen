8eef7866961627e0f726f173e9f74ab3d9811cd8
luisacio <elsantosbcn@gmail.com>
2025-01-13 18:21:59 +0100

    exercici 4 acabat

--- crud/crud.py
+++ crud/crud.py
@@ -1,9 +1,11 @@
 from bbdd.bbdd import connect_to_bbdd
 from psycopg2 import DataError,DatabaseError
 
+#insertar usuari nou
 def insert_user(form):
-    cur_conn = connect_to_bbdd()
+
     try:
+        cur_conn = connect_to_bbdd()
         cur_conn[0].execute("""INSERT INTO public.users_db (nombre
                                                                 ,apellido
                                                                 ,correo_electronico
@@ -13,7 +15,15 @@ def insert_user(form):
                                                                 ,direccion
                                                                 ,codigo_postal
                                                                 ,password)
-                                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""",(form.theme,form.word))
+                                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""",(form.nombre
+                                                                        ,form.apellido
+                                                                        ,form.correoelectronico
+                                                                        ,form.descripcion
+                                                                        ,form.curso
+                                                                        ,form.anyo
+                                                                        ,form.direccion
+                                                                        ,form.codigopostal
+                                                                        ,form.password))
         cur_conn[1].commit()
         msg = {"msg":"OK"}
         cur_conn[0].close()
@@ -28,3 +38,24 @@ def insert_user(form):
         cur_conn[1].close()
 
     return msg
+
+#Mostrar tots el usuaris
+def select_users():
+
+    try:
+        cur_conn = connect_to_bbdd()
+
+        cur_conn[0].execute("""SELECT * FROM public.users_db
+                                 ORDER BY SURNAME ASC  """)
+        res = cur_conn[0].fetchall()
+        cur_conn[0].close()
+        cur_conn[1].close()
+    except DataError:
+        res = {"msg": DataError}
+        cur_conn[0].close()
+        cur_conn[1].close()
+    except DatabaseError:
+        res = {"msg": DatabaseError}
+        cur_conn[0].close()
+        cur_conn[1].close()
+    return res
--- exercici2.py
+++ exercici2.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from base_model.base_model import Form
-from crud.crud import insert_user
+from crud.crud import insert_user,select_users
+from schema.format_form import format_form
+from typing import List
 
 app = FastAPI()
 
+#insertar usuari nou
 @app.post("/formulari")
 async def add_user(formulari:Form):
     msg = insert_user(formulari)
     return msg
+
+#mostrar tots els usuaris
+@app.get("/users/",response_model=List[dict])
+async def show_users():
+    return format_form(select_users())
